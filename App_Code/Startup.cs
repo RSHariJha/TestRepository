be952a80611b0570cd209bf5614fc4d3fc6e389f
hari.jha@rsystems.com <hari.jha@rsystems.com>
2017-08-14 14:41:13 +0530

    Adding new website

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestRepository.Startup))]
+namespace TestRepository
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
